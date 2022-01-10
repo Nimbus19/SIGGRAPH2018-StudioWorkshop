491df4f2a2bca391ec184a2d3f541d80ee21bff5
Wythe <wythe.wang@wanin.tw>
2022-01-10 17:29:47 +0800

    Comment all script has deprecated API

--- Assets/AddPassAfterOpaque.cs
+++ Assets/AddPassAfterOpaque.cs
@@ -1,18 +1,18 @@
-﻿using UnityEngine.Rendering;
+﻿//using UnityEngine.Rendering;
 
-namespace UnityEngine.Experimental.Rendering.LightweightPipeline
-{
-	[ExecuteInEditMode]
-	public class AddPassAfterOpaque : MonoBehaviour, IAfterOpaquePass
-	{
-		const string k_CustomBlitShader = "Hidden/SIGGRAPH Studio/CustomBlit";
-		public void OnEnable()
-		{
-		}
+//namespace UnityEngine.Experimental.Rendering.LightweightPipeline
+//{
+//	[ExecuteInEditMode]
+//	public class AddPassAfterOpaque : MonoBehaviour, IAfterOpaquePass
+//	{
+//		const string k_CustomBlitShader = "Hidden/SIGGRAPH Studio/CustomBlit";
+//		public void OnEnable()
+//		{
+//		}
 
-		public ScriptableRenderPass GetPassToEnqueue(RenderTextureDescriptor desc, RenderTargetHandle colorHandle, RenderTargetHandle depthHandle)
-		{
-			return null;
-		}
-	}
-}
+//		public ScriptableRenderPass GetPassToEnqueue(RenderTextureDescriptor desc, RenderTargetHandle colorHandle, RenderTargetHandle depthHandle)
+//		{
+//			return null;
+//		}
+//	}
+//}
--- Assets/_Completed/AddPassAfterOpaqueCompleted.cs
+++ Assets/_Completed/AddPassAfterOpaqueCompleted.cs
@@ -1,51 +1,51 @@
-﻿using UnityEngine.Rendering;
+﻿//using UnityEngine.Rendering;
 
-namespace UnityEngine.Experimental.Rendering.LightweightPipeline
-{
-	[ExecuteInEditMode]
-	public class AddPassAfterOpaqueCompleted : MonoBehaviour, IAfterOpaquePass
-	{
-		const string k_CustomBlitShader = "Hidden/SIGGRAPH Studio/CustomBlit";
+//namespace UnityEngine.Experimental.Rendering.LightweightPipeline
+//{
+//	[ExecuteInEditMode]
+//	public class AddPassAfterOpaqueCompleted : MonoBehaviour, IAfterOpaquePass
+//	{
+//		const string k_CustomBlitShader = "Hidden/SIGGRAPH Studio/CustomBlit";
 		
-		public Texture2D m_NyanCatTexture;
-		MyNyanCatPassCompleted m_NyanCat;
+//		public Texture2D m_NyanCatTexture;
+//		MyNyanCatPassCompleted m_NyanCat;
 		
-		Material m_Material;
+//		Material m_Material;
 
-		public void OnEnable()
-		{
-			m_NyanCat = new MyNyanCatPassCompleted();
-			m_Material = CoreUtils.CreateEngineMaterial(Shader.Find(k_CustomBlitShader));
-		}
+//		public void OnEnable()
+//		{
+//			m_NyanCat = new MyNyanCatPassCompleted();
+//			m_Material = CoreUtils.CreateEngineMaterial(Shader.Find(k_CustomBlitShader));
+//		}
 
-		public ScriptableRenderPass GetPassToEnqueue(RenderTextureDescriptor desc, RenderTargetHandle colorHandle, RenderTargetHandle depthHandle)
-		{
-			m_NyanCat.Setup(m_NyanCatTexture, colorHandle.Identifier(), m_Material);
-			return m_NyanCat;
-		}
-	}
+//		public ScriptableRenderPass GetPassToEnqueue(RenderTextureDescriptor desc, RenderTargetHandle colorHandle, RenderTargetHandle depthHandle)
+//		{
+//			m_NyanCat.Setup(m_NyanCatTexture, colorHandle.Identifier(), m_Material);
+//			return m_NyanCat;
+//		}
+//	}
 
-	public class MyNyanCatPassCompleted : ScriptableRenderPass
-	{
-		Texture2D m_NyanCatTexture;
-		RenderTargetIdentifier m_DestinationTarget;
-		Material m_Material;
+//	public class MyNyanCatPassCompleted : ScriptableRenderPass
+//	{
+//		Texture2D m_NyanCatTexture;
+//		RenderTargetIdentifier m_DestinationTarget;
+//		Material m_Material;
 
-		public void Setup(Texture2D texture, RenderTargetIdentifier destination, Material material)
-		{
-			m_NyanCatTexture = texture;
-			m_DestinationTarget = destination;
-			m_Material = material;
-		}
+//		public void Setup(Texture2D texture, RenderTargetIdentifier destination, Material material)
+//		{
+//			m_NyanCatTexture = texture;
+//			m_DestinationTarget = destination;
+//			m_Material = material;
+//		}
 
-		public override void Execute(ScriptableRenderer renderer, ref ScriptableRenderContext context, ref CullResults cullResults, ref RenderingData renderingData)
-		{
-			m_Material.SetTexture("_BlitTex", m_NyanCatTexture);
+//		public override void Execute(ScriptableRenderer renderer, ref ScriptableRenderContext context, ref CullResults cullResults, ref RenderingData renderingData)
+//		{
+//			m_Material.SetTexture("_BlitTex", m_NyanCatTexture);
 
-			CommandBuffer cmd = CommandBufferPool.Get("Render Nyan Cat");
-			cmd.Blit(m_NyanCatTexture, m_DestinationTarget, m_Material);
-			context.ExecuteCommandBuffer(cmd);
-			CommandBufferPool.Release(cmd);
-		}
-	}
-}
+//			CommandBuffer cmd = CommandBufferPool.Get("Render Nyan Cat");
+//			cmd.Blit(m_NyanCatTexture, m_DestinationTarget, m_Material);
+//			context.ExecuteCommandBuffer(cmd);
+//			CommandBufferPool.Release(cmd);
+//		}
+//	}
+//}
